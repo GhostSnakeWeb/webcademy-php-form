0b4e7f58fc9d4973c09f312e817a0fe2724d0892
Vladislav <vladan982004@gmail.com>
2018-12-24 17:44:42 +0500

    Simple form

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php 
+	//echo "<strong>_POST array:</strong> <br>";
+	//print_r($_POST);
+	//echo "<br><br>";
+
+	if (!empty($_POST)) {
+
+		$message = "Вам пришло новое сообщение с сайта: \n" 
+		. "Имя отправителя: " . $_POST['userName'] . "\n"
+		. "Email отправителя: " . $_POST['userEmail'] . "\n"
+		. "Сообщение:\n" . $_POST['message'];
+
+		//Откуда пришло письмо
+		$headers = "Наш сайт: info@webprogrammer.ru";
+
+		$resultMail = mail("vladan982004@gmail.com", "Сообщение с сайта", $message, $headers);	
+
+		if ($resultMail) {
+			header('Location: success.html');
+			//echo "Сообщение отправлено успешно!";
+		} else {
+			echo "Что-то пошло не так! Письмо не отправлено.";
+		}
+	}
+
+	//Функция передачи письма. Необязательные параметры в [] скобках (здесь это доп заголовки и доп параметры)
+	//bool mail (string to, string subject, string message [, string additional_headers [, string additional_parameters]])
+
+ ?>
+
+<form action="index.php" method="post">
+ 	<input type="text" name="userName" placeholder="Ваше имя"><br>
+ 	<input type="text" name="userEmail" placeholder="Ваш Email"><br>
+ 	<textarea name="message" id="" cols="30" rows="10" placeholder="Сообщение"></textarea><br>
+ 	<input type="submit" value="Отправить форму">
+ </form>
